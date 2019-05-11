ec9f532ee229c331d88db1b9c0f909687c9c9331
lelanthran <lelanthran@gmail.com>
2019-05-11 11:06:13 +0200

    WIP: Added more of the environment variables we expect to see.

--- src/xcgi.c
+++ src/xcgi.c
@@ -1,9 +1,21 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "xcgi.h"
 
+const char *xcgi_content_length;
+const char *xcgi_content_type;
+const char *xcgi_context_document_root;
+const char *xcgi_content_prefix;
+const char *xcgi_document_root;
+const char *xcgi_gateway_interface;
+const char *xcgi_hostname;
+const char *xcgi_hosttype;
+const char *xcgi_http_accept;
+const char *xcgi_http_host;
+const char *xcgi_http_user_agent;
 const char *xcgi_document_root;
 const char *xcgi_http_cookie;
 const char *xcgi_http_host;
@@ -11,44 +23,66 @@ const char *xcgi_http_referer;
 const char *xcgi_http_user_agent;
 const char *xcgi_https;
 const char *xcgi_path;
+const char *xcgi_pwd;
 const char *xcgi_query_string;
 const char *xcgi_remote_addr;
 const char *xcgi_remote_host;
 const char *xcgi_remote_port;
 const char *xcgi_remote_user;
 const char *xcgi_request_method;
+const char *xcgi_request_scheme;
 const char *xcgi_request_uri;
 const char *xcgi_script_filename;
 const char *xcgi_script_name;
+const char *xcgi_server_addr;
 const char *xcgi_server_admin;
 const char *xcgi_server_name;
 const char *xcgi_server_port;
+const char *xcgi_server_protocol;
+const char *xcgi_server_signature;
 const char *xcgi_server_software;
+
 
 struct {
    const char *name;
    const char **variable;
 } g_vars[] = {
-      { "DOCUMENT_ROOT",    &xcgi_document_root     },
-      { "HTTP_COOKIE",      &xcgi_http_cookie       },
-      { "HTTP_HOST",        &xcgi_http_host         },
-      { "HTTP_REFERER",     &xcgi_http_referer      },
-      { "HTTP_USER_AGENT",  &xcgi_http_user_agent   },
-      { "HTTPS",            &xcgi_https             },
-      { "PATH",             &xcgi_path              },
-      { "QUERY_STRING",     &xcgi_query_string      },
-      { "REMOTE_ADDR",      &xcgi_remote_addr       },
-      { "REMOTE_HOST",      &xcgi_remote_host       },
-      { "REMOTE_PORT",      &xcgi_remote_port       },
-      { "REMOTE_USER",      &xcgi_remote_user       },
-      { "REQUEST_METHOD",   &xcgi_request_method    },
-      { "REQUEST_URI",      &xcgi_request_uri       },
-      { "SCRIPT_FILENAME",  &xcgi_script_filename   },
-      { "SCRIPT_NAME",      &xcgi_script_name       },
-      { "SERVER_ADMIN",     &xcgi_server_admin      },
-      { "SERVER_NAME",      &xcgi_server_name       },
-      { "SERVER_PORT",      &xcgi_server_port       },
-      { "SERVER_SOFTWARE",  &xcgi_server_software   },
+      { "CONTENT_LENGTH",           &xcgi_content_length          },
+      { "CONTENT_TYPE",             &xcgi_content_type            },
+      { "CONTEXT_DOCUMENT_ROOT",    &xcgi_context_document_root   },
+      { "CONTEXT_PREFIX",           &xcgi_content_prefix          },
+      { "DOCUMENT_ROOT",            &xcgi_document_root           },
+      { "GATEWAY_INTERFACE",        &xcgi_gateway_interface       },
+      { "HOSTNAME",                 &xcgi_hostname                },
+      { "HOSTTYPE",                 &xcgi_hosttype                },
+      { "HTTP_ACCEPT",              &xcgi_http_accept             },
+      { "HTTP_HOST",                &xcgi_http_host               },
+      { "HTTP_USER_AGENT",          &xcgi_http_user_agent         },
+      { "DOCUMENT_ROOT",            &xcgi_document_root           },
+      { "HTTP_COOKIE",              &xcgi_http_cookie             },
+      { "HTTP_HOST",                &xcgi_http_host               },
+      { "HTTP_REFERER",             &xcgi_http_referer            },
+      { "HTTP_USER_AGENT",          &xcgi_http_user_agent         },
+      { "HTTPS",                    &xcgi_https                   },
+      { "PATH",                     &xcgi_path                    },
+      { "PWD",                      &xcgi_pwd                     },
+      { "QUERY_STRING",             &xcgi_query_string            },
+      { "REMOTE_ADDR",              &xcgi_remote_addr             },
+      { "REMOTE_HOST",              &xcgi_remote_host             },
+      { "REMOTE_PORT",              &xcgi_remote_port             },
+      { "REMOTE_USER",              &xcgi_remote_user             },
+      { "REQUEST_METHOD",           &xcgi_request_method          },
+      { "REQUEST_SCHEME",           &xcgi_request_scheme          },
+      { "REQUEST_URI",              &xcgi_request_uri             },
+      { "SCRIPT_FILENAME",          &xcgi_script_filename         },
+      { "SCRIPT_NAME",              &xcgi_script_name             },
+      { "SERVER_ADDR",              &xcgi_server_addr             },
+      { "SERVER_ADMIN",             &xcgi_server_admin            },
+      { "SERVER_NAME",              &xcgi_server_name             },
+      { "SERVER_PORT",              &xcgi_server_port             },
+      { "SERVER_PROTOCOL",          &xcgi_server_protocol         },
+      { "SERVER_SIGNATURE",         &xcgi_server_signature        },
+      { "SERVER_SOFTWARE",          &xcgi_server_software         },
 };
 
 bool xcgi_init (void)
@@ -122,6 +156,7 @@ bool xcgi_load (const char *fname)
          goto errorexit;
       }
       *tmp++ = 0;
+      // TODO: For Windows must use putenv_s()
       setenv (line, tmp, 1);
    }
    // TODO: Read stdin?
--- src/xcgi.h
+++ src/xcgi.h
@@ -26,6 +26,17 @@ extern "C" {
 // All of these variables are non-NULL after a successful xcgi_init(). The
 // caller MUST NOT modify the variables.
 
+extern const char *xcgi_content_length;
+extern const char *xcgi_content_type;
+extern const char *xcgi_context_document_root;
+extern const char *xcgi_content_prefix;
+extern const char *xcgi_document_root;
+extern const char *xcgi_gateway_interface;
+extern const char *xcgi_hostname;
+extern const char *xcgi_hosttype;
+extern const char *xcgi_http_accept;
+extern const char *xcgi_http_host;
+extern const char *xcgi_http_user_agent;
 extern const char *xcgi_document_root;
 extern const char *xcgi_http_cookie;
 extern const char *xcgi_http_host;
@@ -33,18 +44,23 @@ extern const char *xcgi_http_referer;
 extern const char *xcgi_http_user_agent;
 extern const char *xcgi_https;
 extern const char *xcgi_path;
+extern const char *xcgi_pwd;
 extern const char *xcgi_query_string;
 extern const char *xcgi_remote_addr;
 extern const char *xcgi_remote_host;
 extern const char *xcgi_remote_port;
 extern const char *xcgi_remote_user;
 extern const char *xcgi_request_method;
+extern const char *xcgi_request_scheme;
 extern const char *xcgi_request_uri;
 extern const char *xcgi_script_filename;
 extern const char *xcgi_script_name;
+extern const char *xcgi_server_addr;
 extern const char *xcgi_server_admin;
 extern const char *xcgi_server_name;
 extern const char *xcgi_server_port;
+extern const char *xcgi_server_protocol;
+extern const char *xcgi_server_signature;
 extern const char *xcgi_server_software;
 
 #endif
