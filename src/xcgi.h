8d4813f2f9aeeac3fe44e56ddb39bb49ee6f77be
lelanthran <lelanthran@gmail.com>
2019-05-12 00:27:17 +0200

    WIP: Specifying an API for how query strings will work.

--- src/xcgi.h
+++ src/xcgi.h
@@ -15,6 +15,9 @@
 extern "C" {
 #endif
 
+   //////////////////////////////////////////////////////////////////
+
+
    // Initialises the cgi variables. Returns true on success, false on error.
    //
    // NOTE: This function does not load the query strings nor does it read
@@ -34,6 +37,10 @@ extern "C" {
    // course of execution of this library.
    void xcgi_shutdown (void);
 
+
+   //////////////////////////////////////////////////////////////////
+
+
    // Load/save the cgi environment for later playback.
    bool xcgi_load (const char *fname);
    bool xcgi_save (const char *fname);
@@ -41,6 +48,10 @@ extern "C" {
    // Return the value of the http variable specified, never NULL. Caller
    // must not free the result.
    const char *xcgi_getenv (const char *name);
+
+
+   //////////////////////////////////////////////////////////////////
+
 
    // Returns a copy of the specified string with all the non-ascii
    // characters replaced with their hex equivalent using %xx as the
@@ -60,8 +71,43 @@ extern "C" {
    // failure NULL is returned. The caller must free the result.
    char *xcgi_string_unescape (const char *src);
 
+
+   //////////////////////////////////////////////////////////////////
+
+
+   // When parsing query strings, the library will refuse to parse POST
+   // request query strings that do not match an acceptable content-type.
+   // These two functions add and remove acceptable content-type
+   // specifications from the whitelist used by the parser.
+   //
+   // The list is initially empty, so all POST query strings will be
+   // ignored.
+   //
+   // The function xcgi_qstrings_accept_content_type() adds a content-type
+   // specification to the list. This function can be called multiple
+   // times, even with the same content-type, with no ill-effect. This
+   // function must be called at least once for each content-type that
+   // can be parsed as a POST request query string.
+   //
+   // The function xcgi_qstrings_reject_content_type() removes content type
+   // specifications from the list of acceptable content types for query
+   // strings. This function can be called multiple times, even with the
+   // same content-type, with no ill effects.
+   //
+   // On success true is returned. On error false is returned. Adding
+   // duplicated content-types is not an error (returns true) and removing
+   // non-existing content-types is not an error (returns true).
+   bool xcgi_qstrings_accept_content_type (const char *content_type);
+   bool xcgi_qstrings_remove_content_type (const char *content_type);
+
    // Parse the query strings into memory. Returns true on success and
-   // false on error (for example, running out of memory).
+   // false on error (for example, running out of memory). Both GET and
+   // POST query strings are checked, with an added check on the
+   // CONTENT_TYPE for ensuring that POST query strings are of the correct
+   // content-type.
+   //
+   // Set acceptable query strings content-types using
+   // xcgi_qstrings_accept_content_type().
    //
    // The caller can iterate across the query strings by using
    // xcgi_qstrings_count() to get a count of the query strings and
@@ -72,7 +118,14 @@ extern "C" {
    // name/value pairs is terminated with a NULL and can be looped over
    // until the NULL is encountered.
    bool xcgi_qstrings_parse (void);
+
+   // Return the number of query strings found. Must be called only after
+   // a successful call to xcgi_qstrings_parse().
    size_t xcgi_qstrings_count (void);
+
+   // Return an array of char *[2], terminated with a NULL pointer, that
+   // contains all the query strings found by xcgi_qstrings_parse(). Must
+   // be called only after a successful call to xcgi_qstrings_parse()
    const char ***xcgi_qstrings (void);
 
 #ifdef __cplusplus
