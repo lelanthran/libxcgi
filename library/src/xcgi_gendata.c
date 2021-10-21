208f6b88a4057a80f6467121664c938b1ad8fdbb
lelanthran <lelanthran@gmail.com>
2021-10-21 12:39:31 +0200

    Updated Makefile to use INSTALL_PREFIX

--- library/src/xcgi_gendata.c
+++ library/src/xcgi_gendata.c
@@ -56,7 +56,7 @@ int main (void)
 
    static const size_t nvars = sizeof vars / sizeof vars[0];
 
-   printf ("Test: libxcgi v%s\n", XCGI_VERSION);
+   printf ("Test: libxcgi v%s\n", xcgi_version);
 
    for (size_t i=0; i<nvars; i++) {
       vars[i].value = (*vars[i].variable);
